06cc04be503562c858fef266563a3ed414907619
Ergülü Bozkurt <ergulubozkurt5@gmail.com>
2024-02-04 15:05:01 +0300

    Update try.py

--- try.py
+++ try.py
@@ -1,30 +1,9 @@
-import picamera
+from picamera2 import Picamera2, Preview
 import time
-
-def video_ceki(video_suresi):
-    try:
-        # Kamera nesnesini başlat
-        with picamera.PiCamera() as camera:
-            # Video çözünürlüğünü ayarla (isteğe bağlı)
-            # camera.resolution = (width, height)
-            
-            # Video kaydının başlamasını bekle
-            time.sleep(2)
-            
-            # Video çekimini başlat
-            video_adı = 'kayit.mp4'
-            camera.start_recording(video_adı)
-            
-            # Belirlenen süre kadar video çekimini devam ettir
-            camera.wait_recording(video_suresi)
-            
-            # Video çekimini durdur
-            camera.stop_recording()
-            
-            print(f"{video_suresi} saniyelik video başarıyla kaydedildi: {video_adı}")
-
-    except Exception as e:
-        print(f"Hata oluştu: {str(e)}")
-
-# 5 saniyelik video çekimi için
-video_ceki(5)
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+time.sleep(2)
+picam2.capture_file("test.jpg")
